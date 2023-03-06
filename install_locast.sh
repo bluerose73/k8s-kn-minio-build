86ec6f2b3884cd0a2be1a6f6684c9797eab432eb
Shengjie Ma <2544747588@qq.com>
2023-03-06 19:28:01 +0800

    Update install_locast.sh

--- install_locast.sh
+++ install_locast.sh
@@ -7,7 +7,7 @@ sudo apt-get install -y python3-pip
 
 pip3 install locust
 
-echo "\n\n" >> ~/.bashrc
+echo -e "\n\n" >> ~/.bashrc
 echo 'export PATH="/users/bluerose/.local/bin:$PATH"' >> ~/.bashrc
 
 locust -V
